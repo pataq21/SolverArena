b7f9106c29decd56138a5743b6b33258d5fd3fd5
pataq21 <pataq21@gmail.com>
2024-09-29 19:44:24 +0200

    add example to test

---
+++ example.py
@@ -0,0 +1,9 @@
+from run import run_models, save_results_to_csv
+
+
+if __name__ == "__main__":
+    mps_files = ["mps_files/model_dataset100.mps"]
+    solvers = ["highs", "gurobi"]
+
+    results = run_models(mps_files, solvers)
+    save_results_to_csv(results)
