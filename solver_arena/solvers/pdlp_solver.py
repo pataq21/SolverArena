111ddc9f820f392c3d4aa64fe8d6069144e5a648
pataq21 <pataq21@gmail.com>
2024-10-02 18:29:46 +0200

    Rework pdlp and scip

--- solver_arena/solvers/pdlp_solver.py
+++ solver_arena/solvers/pdlp_solver.py
@@ -1,43 +1,80 @@
-import time
-import psutil
-from ortools.linear_solver import pywraplp
+from datetime import datetime
+import logging
+
 from ortools.linear_solver.python import model_builder
 
 from solver_arena.solvers.solver import Solver
+from solver_arena.solvers.utils import track_performance
 
 
 class PDLPSolver(Solver):
     def __init__(self):
+        """
+        Initializes the solver with an empty result.
+        """
         self.result = None
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
 
-    def solve(self, mps_file, time_limit=1500):
-        model = model_builder.ModelBuilder()
-        model.import_from_mps_file(mps_file)
-        solver = model_builder.ModelSolver('PDLP')
-        solver.set_time_limit_in_seconds(time_limit)
+    @track_performance
+    def run_pdlp(self, pdlp, model):
+        """
+        Runs the PDLP solver and tracks performance using the track_performance decorator.
 
-        start_time = time.time()
-        memory_before = psutil.Process().memory_info().rss / (1024 * 1024)
-        status = solver.solve(model)
-        end_time = time.time()
-        memory_after = psutil.Process().memory_info().rss / (1024 * 1024)
+        Args:
+            pdlp (ModelSolver): The PDLP solver instance.
+            model (ModelBuilder): The instance model.
 
-        if status == pywraplp.Solver.OPTIMAL:
-            obj_value = solver.objective_value
-        else:
-            obj_value = None
+        Returns:
+            dict: A dictionary containing the solver status and objective value.
+        """
+        model_status = pdlp.solve(model)
 
-        run_time = end_time - start_time
+        obj_value = pdlp.objective_value
 
-        self.result = {
-            "status": status,
+        return {
+            "status": model_status,
             "objective_value": obj_value,
-            "runtime": run_time,
-            "solver": "glop",
-            "memory_before_MB": memory_before,
-            "memory_after_MB": memory_after,
-            "memory_used_MB": memory_after - memory_before,
+            "solver": "PDLP"
         }
 
+    def solve(self, mps_file, options=None):
+        """
+        Solves the optimization problem using the PDLP solver.
+
+        Args:
+            mps_file (str): The path to the MPS file containing the model.
+            options (dict, optional): A dictionary of solver options to configure PDLP.
+
+        Raises:
+            FileNotFoundError: If the provided MPS file does not exist.
+            ValueError: If an invalid option is passed in the options dictionary.
+        """
+        model = model_builder.ModelBuilder()
+        model.import_from_mps_file(mps_file)
+        pdlp = model_builder.ModelSolver('PDLP')
+
+        if options:
+            for key, value in options.items():
+                if key == 'time_limit':
+                    pdlp.set_time_limit_in_seconds(value)
+                else:
+                    self.logger.info(f"Parameter {key} is not implemented or it does not exist")
+
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.logger.info(f"[{current_time}] Running the PDLP solver on {mps_file}...")
+
+        self.result = self.run_pdlp(pdlp, model)
+
+        self.logger.info(f"Solver completed with status: {self.result['status']}.")
+
     def get_results(self):
+        """
+        Returns the result of the last solver run.
+
+        Returns:
+            dict: A dictionary containing the results of the solver run.
+        """
+        if self.result is None:
+            self.logger.warning("No problem has been solved yet. The result is empty.")
         return self.result
